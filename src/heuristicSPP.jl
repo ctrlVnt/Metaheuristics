e7cb10b832dea7f72b6281f2d6ed952705d19c38
ctrlVnt <gordanobruno227@gmail.com>
2025-10-08 15:56:16 +0200

    changed heuristic

--- src/heuristicSPP.jl
+++ src/heuristicSPP.jl
@@ -11,13 +11,13 @@ function heuristicSPP(C, A)
 
     # greedy construction
     used_rows = falses(m)   # vector of toke constrancts
-    order = sortperm(C, rev=true)  # we order index of proficts in decreasing order (Not indispensable)
-    #order = 1:n # natural index order
+    #order = sortperm(C, rev=true)  # we order index of proficts in decreasing order (Not indispensable)
+    order = 1:n # natural index order
     for i in order
-        rows_un = findall(!iszero, A[:, i])  # we create a vector with the number of index in column i that are 1
-        if !any(used_rows[rows_un])          # if in rows_un positions of contrancts vector we don't find true
+        rows_of_one = findall(!iszero, A[:, i])  # we create a vector with the number of index in column i that are 1
+        if !any(used_rows[rows_of_one])          # if in rows_of_one positions of contrancts vector we don't find true
             x[i] = 1                      # we insert this variable in solution vector S ← S ∪ {i}
-            used_rows[rows_un] .= true       # we update position of toke constrants
+            used_rows[rows_of_one] .= true       # we update position of toke constrants
         end
     end
 
